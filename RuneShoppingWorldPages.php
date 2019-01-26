89adf3d7d9a7582ce75168222c67cc6dc4aeb54f
Rune Laenen <rune@laenen.nu>
2019-01-26 22:24:51 +0100

    First commit

---
+++ RuneShoppingWorldPages.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace RuneShoppingWorldPages;
+
+use Shopware\Bundle\AttributeBundle\Service\CrudService;
+use Shopware\Bundle\AttributeBundle\Service\TypeMapping;
+use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\InstallContext;
+use Shopware\Models\Emotion\Emotion;
+
+class RuneShoppingWorldPages extends Plugin
+{
+    /**
+     * @param InstallContext $context
+     * @throws \Exception
+     */
+    public function install(InstallContext $context)
+    {
+        /** @var CrudService $crudService */
+        $crudService = $this->container->get('shopware_attribute.crud_service');
+
+        $crudService->update('s_cms_static_attributes',
+            'rune_shoppingworldpage',
+            'single_selection',
+            [
+                'label' => 'Shopping World',
+                'displayInBackend' => true,
+                'translatable' => true,
+                'helpText' => 'Will replace Shop Page content.',
+                'columnType' => TypeMapping::TYPE_SINGLE_SELECTION, 'entity' => Emotion::class,
+                'position' => 0,
+                'custom' => 'false'
+            ],
+            null,
+            true);
+
+        parent::install($context);
+    }
+}
